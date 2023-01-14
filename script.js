fec80696a9367160dd136be876e72215d2d661e8
Evan Vargas <evanvargas@gmail.com>
2023-01-13 23:11:22 -0600

    Phase 1 linkedlists

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ User.prototype.emailDomain = '@facebook.com';
 User.prototype.scoobyDoo = function() {
     return this.firstName + ' ' + this.lastName + 'We hate arrow funcitons!'
 };
+
+
+//A linked list
+//Singly link list has only reference ahead of it
+//In a doubly linked list, each node has before and after
+
+//Setting head and tail properties to null by leaving it blank and setting it to null.
+//You create instances of a constructor function
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+var node1 = new Node(100, 'node2', null);
